b9afcfec95b519089793686a70f84ba615e4f6cd
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2024-11-05 19:04:26 -0600

    Cambio dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,22 +5,6 @@
         </h2>
     </x-slot>
 
-    <section class="bg-white dark:bg-gray-900">
-	<div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-		<div class="mr-auto place-self-center lg:col-span-7">
-			<h1
-				class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-				Turismo <br>sin Mapas.</h1>
-				<p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Explora el mundo con nosotros: en nuestro sitio web de viajes encontrarás las mejores recomendaciones para destinos inolvidables, guías prácticas, y consejos de expertos para que planifiques el viaje perfecto. Desde escapadas urbanas hasta aventuras en la naturaleza, nos encargamos de ofrecerte inspiración y recursos para que tu próxima experiencia sea única y sin complicaciones. ¡Comienza a descubrir el mundo hoy!<a
-						href="#" target="_blank" class="hover:underline"></a><a href="#/"
-						class="hover:underline" target="_blank"></a> <a href="https://flowbite.com/blocks/" target="_blank"
-						class="hover:underline" target="_blank"></a>.</p>
-		</div>
-		<div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-			<img src="{{asset('imagenesMich/inicio.jpg')}}" alt="hero image">
-</div>
-		</div>
-</section> <!-- Termina la seccion de bienvenida -->
 
 <!-- INICIA CARRUSEL -->
 
